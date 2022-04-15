cd7618d6f7ba0ff5b1106038433dcaa93c96c1b5
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2022-04-15 13:39:10 +0700

    Update mlb.js

--- Commands/economy/mlb.js
+++ Commands/economy/mlb.js
@@ -11,19 +11,21 @@ module.exports = {
                 const bal = await client.bal(id);
                 console.log(`${member.user.tag} -> ${bal}`);
                 return bal !== 0 ? collection.set(id, {
-                        id,
-                        bal,
-                    }) :
-                    null;
+                    id,
+                    bal,
+                }) : null;
             })
         );
         const data = collection.sort((a, b) => b.bal - a.bal).first(10);
-
-        message.channel.send(
-            new MessageEmbed()
-            .setTitle(`Bảng xếp hạng của ${message.guild.name}`)
-            .setColor('GREEN')
-            .setDescription(data.map((v, i) => { return `${i+1}) ${client.users.cache.get(v.id).tag} => **${v.bal} coins**`; }))
-        )
+        let lb = await data.map((v, i) => {
+            return `\`#${i+1}\`| ${client.users.cache.get(v.id).tag} - \**${v.bal}\** <:phmoney:964044114289127484>`;
+        })
+        const embed = new MessageEmbed()
+            .setAuthor({ name: `Bảng xếp hạng PH Money - ${client.user.username}`, iconURL: message.guild.iconURL({ dynamic: true }) })
+            .setColor('BLURPLE')
+            .setDescription(lb.join("\n"))
+            .setTimestamp()
+            .setFooter({ text: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+        message.channel.send({ embeds: [embed] })
     },
 };
